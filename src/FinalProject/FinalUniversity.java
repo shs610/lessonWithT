772ec763cb8233c21106388d42c48a1c992ec036
shs610 <gustns5501@naver.com>
2024-12-31 15:05:48 +0900

    Update FinalUniversity.java

--- src/FinalProject/FinalUniversity.java
+++ src/FinalProject/FinalUniversity.java
@@ -2,17 +2,23 @@ package FinalProject;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-class classStudent {
+class classStudent {  // 학생 클래스 - DB 역할
     private int studentNumber;  // 학번
     private String studentName;  // 이름
-    ArrayList<classGrade> studentGrade;  // 성적리스트
+    ArrayList<classGrade> studentGrade;  // 성적리스트(Arraylist)
+    private String subject;  // 과목
     ArrayList<classGrade> getstudentGrade() {return studentGrade;}  // get 성적
     String getName() {return studentName;}  // getName 이름
     int getNumber() {return studentNumber;}  // getNumber 학번
+    String getSubject() {return subject;}  // getSubject 과목
     void setName(String studentName) {this.studentName = studentName;}  // setName 이름
-    void setNumber(int studentNumber) {this.studentNumber = studentNumber;}  //getNumber 학번
+    void setNumber(int studentNumber) {this.studentNumber = studentNumber;}  // getNumber 학번
     void setstudentGrade(ArrayList<classGrade> studentGrade) {this.studentGrade = studentGrade;}  // set 성적
-
+    void setSubject(String subject) {this.subject = subject;}  // set 과목
+    classStudent(int studentNumber, String studentName) {
+        this.studentNumber = studentNumber;
+        this.studentName = studentName;
+    }
 }
 
 class classGrade {
@@ -26,11 +32,18 @@ class classGrade {
 
 public class FinalUniversity {
     public static void main(String[] args) {
-        ArrayList<classStudent> students = new ArrayList<>();
-        classStudent s = new classStudent();
+        ArrayList<classStudent> students = new ArrayList<>();  // 학생 정보를 넣을 리스트
+        classStudent s = new classStudent(202412345, "Amy");
         students.add(s);
         Scanner sc = new Scanner(System.in);
-        String id = sc.next();
+        System.out.println("id를 입력하세요");
+        int id = sc.nextInt();
 
+
+        for(classStudent st : students){
+            if (id == st.getNumber()) {
+                System.out.println("name : " + st.getName() + ", " + "subject : " + st.getSubject());
+            }
+        }
     }
 }
